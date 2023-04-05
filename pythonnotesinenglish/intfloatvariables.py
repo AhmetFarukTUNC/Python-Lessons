071bdc5f0a40ee05b89af78c4cdcac98af0cc59a
AhmetFarukTUNC <129683968+AhmetFarukTUNC@users.noreply.github.com>
2023-04-06 01:35:59 +0300

    Updated intfloatvariables.
    Updated intfloatvariables.

--- pythonnotesinenglish/intfloatvariables.py
+++ pythonnotesinenglish/intfloatvariables.py
@@ -1,8 +1,8 @@
-# Integer(int) değişkenler tamsayıları tutmamızı sağlayan veri tipleridir.
+# Integer(int) variables are data types that allow us to hold integers.
 
-# Float değişkenler rasyonel sayıları tutmamızı sağlayan veri tipleridir.
+# Float variables are data types that allow us to hold rational numbers.
 
-#Matematiksel işlemler
+#Mathematical operations
 
 print(2+3) # 5 int 2 int 3 int
 
@@ -10,14 +10,13 @@ print(2*1.5) # 3.0 float 2 int 1.5 float
 
 print(2*0.5) # 1.0 float 2 int 0.5 float
 
-print(type(2)) # 2'nin türü int
+print(type(2)) # 2's type is int
 
-print(type(2.0)) #2.0'ın türü float
+print(type(2.0)) #2.0's type is float
 
 print(2+10**10+8) # 10*10*10*10*10*10*10*10*10*10 + 2+ 8 = 10.000.000.010
 
 print((2+10)**10+8) # 12*12*12*12*12*12*12*12*12*12 +8 = 743.008.370.696
-
 
 
 
