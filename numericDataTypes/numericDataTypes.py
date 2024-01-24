b8bde9a0f5120df3230bdea23ed8076846226b44
YAZILIMCIABII <atunc8402@gmail.com>
2024-01-24 15:27:39 +0300

    Numeric Data Types
    We did examples about int,str,float and complex data types.

---
+++ numericDataTypes/numericDataTypes.py
@@ -0,0 +1,21 @@
+print(2 + 2) # It prints sum of 2 and 2.
+
+# => We use this icon for create comment row.
+
+x = 2 # x is equal to 2.
+
+print(type(x)) # It prints data type of x.
+
+print(type(x + 0.5)) # It prints data type of sum of x and 0.5.It returns float because sum of x and 0.5 is equal to 2.5 so float.
+
+letter = "A"
+
+print(type(letter)) # It prints data type of letter variable. Data type of letter variable is string(str).
+
+complexNumber = 2 + 3j
+
+print(type(complexNumber)) # It prints that complex number variable is complex as data type.
+
+print(complexNumber.real) # It prints real part of value of complex number.
+
+print(complexNumber.imag) # It prints imaginary part of value of complex number.
