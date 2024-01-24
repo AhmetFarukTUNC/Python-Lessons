dce25361871ff184998bd92ee9a2f9343eef6be9
Ahmet Faruk TUNC <129683968+AhmetFarukTUNC@users.noreply.github.com>
2024-01-24 14:35:54 +0300

    Update applogic.py

--- applogic/applogic.py
+++ applogic/applogic.py
@@ -1,3 +1,3 @@
 print("Hello world!") # It prints to screen "Hello world!" expression.
 
-# You write your code and py pr python and space applogic.py and It runs in terminal that you used
+# You write your code and py or python and space applogic.py and It runs in terminal that you used
