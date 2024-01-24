270c50187cd2535202d53302e56b0204dd8e32c2
YAZILIMCIABII <atunc8402@gmail.com>
2024-01-24 14:44:28 +0300

    Variables In Python
    We did general examples about variables.

---
+++ variables/variables.py
@@ -0,0 +1,29 @@
+x = 2 # x is equal to 2.
+
+print(x) # It prints x.
+
+x = 12 # It was 2 but x happened 12.
+
+print(x) # It prints x.
+
+a = 1 # a is equal to 1.
+
+print(a) # It prints a.
+
+A = 2 # A is equal to 2
+
+print(A) # It prints A.
+
+x = 5 # x is equal to 5.
+
+y = 107 # y is equal to 107.
+
+print(x + y) # It prints sum of x and y.
+
+x = "5" # x is equal to 5.But 5 is string so character
+
+Y="ANLAŞILIR" # Y is equal to "ANLAŞILIR" string expression.
+
+print(x + Y) # It prints combination of x and Y.
+
+print(x+y) # It returns error because y is integer so number(5) but y is not number anymore so character.
