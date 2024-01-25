cba54c9a2ea4ee5217d38ae33ac46064cbea9212
YAZILIMCIABII <atunc8402@gmail.com>
2024-01-25 11:42:55 +0300

    Textual Data Types
    We did examples about textual data types.

---
+++ textualdatatypes/textualdatatypes.py
@@ -0,0 +1,44 @@
+print("Understandable Software") # It prints to screen "Understandable software" textual expression.
+
+x = "Understandable Software"# It assigns to x the "Understandable Software" textual expression.
+
+print(x) # It prints x.
+
+print(type(x)) # It prints type of x.
+
+print(x[0]) # It prints value of 0. index.
+
+print(x[5]) # It prints value of 5. index.
+
+print(x[14]) # It prints value of 14. index.
+
+# We looped as long as the character length of x and printed the character at that index of x in each loop.
+
+for i in range(len(x)):
+    print(x[i])
+
+print("") # It moves to the bottom line.
+
+print(x[-2]) # It prints value of 2nd the last index.
+
+print(x[0:10]) # It prints 10 characters starting from 0.index.
+
+print(x[0:10:2]) # Go 2 by 2 starting from 0 but get 10 characters
+
+a = "Understandable" # a is equal to Understandable textual expression. 
+
+b = "Software" # b is equal to Software textual expression.
+
+r = a + " " + b # r is equal to combination of a,space and b 
+
+print(r) # It prints r.
+
+print(3 * a) # It prints to a 3 times.
+
+print(a + "\n" + b) # It prints a under b.
+
+print(len(x)) # It prints cahaacter length of x.
+
+print(x[22]) # It prints value of last index.
+
+print(x[len(x) - 1]) # It prints value of last index as use len function.
