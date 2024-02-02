be8a1d19ab77e4ee5c661dfb76c3eab67e2a0de5
YAZILIMCIABII <atunc8402@gmail.com>
2024-02-02 23:37:12 +0300

    Added Tuple Files
    We added tuple files.

---
+++ Tuples/tuples.py
@@ -0,0 +1,19 @@
+list1 = ["Ahmet",23] # We defined ist here.
+
+print(type(list1)) # We printed type of this list.
+
+tuple1 = ("Ahmet") # We defined a tuple that there are 1 element
+
+print(type(tuple1)) # We printed tuple.
+
+tuple2 = ("Ahmet",23) # We defined second tuple that there are two elements. 
+
+print(type(tuple2)) # We printed this tuple.
+
+list1[0] = "Mehmet" # We assigns mehmet expression to first index of list1. 
+
+print(list1) # We printed list1 variable.
+
+print(tuple2[0]) # We printed first index of second tuple.
+
+#bundle2[0] = "Mehmet" # We assigned Mehmet expression to first index of second tuple but unsuccess because there are not assignment in tuple.
