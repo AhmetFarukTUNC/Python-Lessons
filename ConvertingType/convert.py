e0da5ced956402d51965c7efbe6d29148a001fd1
YAZILIMCIABII <atunc8402@gmail.com>
2024-02-05 20:35:14 +0300

    Last Title : Type Conversion
    We added type conversion documents.

---
+++ ConvertingType/convert.py
@@ -0,0 +1,40 @@
+a = 2 # a is equal to 2.
+
+b = 2.6 # b is equal to 2.6
+
+c = "24" # c is equal to 24 that be string
+
+print(type(a)) # We printed type of a.
+
+print(type(b)) # We printed type of b.
+
+print(type(c)) # We printed type of c.
+
+d = str(a) # We are converting a to string and assigning to d.
+
+print(d + c) # We are summing up D and C and printing the result.
+
+print(type(d)) # We printed type of result.
+
+e = float(c) # We are converting c to float and assigning to e.
+
+print(e + b) # We are summing e and b and printing to result.
+
+print(type(e)) # We are printing type of e to screen that be converted to flaot of c.
+
+x = [1,3,5,7,9] # We defined a array.There are 5 element as 1,3,5,7,9 in this array.
+
+print(type(x)) # We printed type of this array.
+
+y = tuple(x) # We converting array to tuple.Elements are same.
+
+print(type(y)) # We printed type of y.
+
+print(y) # We printed y that be converted tuple of x. 
+
+z = set(x) # We converting x to set and assigns to z.
+
+print(z) # We printed z variable.
+
+print(type(z)) # We printed type of z variable.
+
