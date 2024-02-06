e3324f532a69c2d062f71dd92685ff28c9d2e820
YAZILIMCIABII <atunc8402@gmail.com>
2024-02-06 18:52:56 +0300

    added and paired documents
    We added documents and paired documnents...

---
+++ InputFunction/inputt.py
@@ -0,0 +1,18 @@
+x = input("What Is The Number? : ") # We took input and assigned value of input to x.
+
+y = input("What Is The Number? : ") # We took input and assigned vakue of input to x.
+
+z = float(x) + float(y) # We converted x and y to float and assigned result to z.
+
+print("Result : " + str(z)) # We printed the stringified version of the result.
+
+diameter = input("Input value of diameter : ") # We took diameter from user.
+
+radius = float(diameter) / 2 # We found radius with divide to two.
+
+pi = 3.14159 # We defined pi as const
+
+area = pi * radius * radius # We found area of circle.
+
+print("Area of Circle is " + str(area)) # We printed area of circle.
+
