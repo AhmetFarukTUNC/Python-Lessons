3f52fd7ff9d06bd1f90b2cd5aaacf4211000f64c
YAZILIMCIABII <atunc8402@gmail.com>
2024-02-22 16:29:32 +0300

    We added Functions folder.

---
+++ Functions/functions.py
@@ -0,0 +1,19 @@
+def listCredits():
+    credits = ["Fast Credit","Particular to paycheck from Jenniferbank","Happy retirement consumer loan"]
+
+    for credit in credits:
+       print(credit)
+
+listCredits()
+
+print()
+
+listCredits()
+
+print()
+
+listCredits()
+
+print()
+
+listCredits()
