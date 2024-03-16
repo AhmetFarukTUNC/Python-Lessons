e559910cbae1281960679a551a82b7339ed968de
YAZILIMCIABII <atunc8402@gmail.com>
2024-03-16 23:43:24 +0300

    We updated 16th March in 2024

---
+++ IfElseIfElseStatement/ReadMe.md
@@ -0,0 +1,43 @@
+# Login and Dollar Comparison
+
+## This Python script performs a basic login authentication and compares yesterday's and today's dollar 
+
+## rates, printing arrows to indicate the direction of change.
+
+## Usage
+
+### 1. Clone the repository:
+
+#### git clone https://github.com/AhmetFarukTUNC/Python-Lessons/
+
+### 2. Navigate to the directory:
+
+### cd your_repository
+
+#### Run the script:
+
+##### python ifelseifelse.py
+
+#### Follow the prompts to enter your username and password.
+
+#### Authentication
+
+##### If the username is "Understandable Economy" and the password is "1234567", it prints "Login is 
+
+##### successful, welcome".
+    
+##### If the username is incorrect but the password is "1234567", it prints "Username is wrong".
+    
+##### If the username is "Understandable Economy" but the password is incorrect, it prints "Password is ##### wrong".
+##### If both the username and password are incorrect, it prints "Username and password are wrong".
+
+## Dollar Comparison
+
+### The script compares yesterday's and today's dollar rates.
+### If yesterday's rate is less than today's rate, it prints "increasing arrow".
+### If yesterday's rate is greater than today's rate, it prints "decreasing arrow".
+### If both rates are equal, it prints "equal arrow".
+
+## Requirements
+
+### Python 3.x
--- IfElseIfElseStatement/ifelseifelse.py
+++ IfElseIfElseStatement/ifelseifelse.py
@@ -1,30 +1,44 @@
-username = "Understandable Economyy"
+username = "Understandable Economyy" # username is equal to "Understandable Economyy" 
 
-password = "12345678"
+password = "12345678" # password is equal to 12345678.
+
+# If username is Understandable Economy and password is 1234567,then,It is printed expression that Login is success,welcome
 
 if username == "Understandable Economy" and password == "1234567":
     print("Login is success,welcome")
 
+# If username is not Understandable Economy and password is 1234567,then,It is printed expression that username is wrong.
+
 elif not(username == "Understandable Economy") and password == "1234567":
     print("Username is wrong")
+
+# If username is Understandable Economy and password is not 1234567,then,It is printed expression that password is wrong.
 
 elif not(password == "1234567") and username == "Understandable Economy":
     print("Password is wrong.")
 
+# Otherwise,It is printed expression that username and password is wrong.
+
 else : 
     print("Username and password is wrong.")
 
-dolarYesterday = 31.
+dolarYesterday = 31.23 # dolarYesterday is equal to 31.23. 
 
-dolarToday = 31.75
+dolarToday = 31.75 # dolarToday is equal to 31.75.
+
+# If dolarYesterday is lesser than dolarToday,then,it is printed expression that increasing arrow.
 
 if dolarYesterday < dolarToday:
     print("increasing arrow")
 
+# If dolarYesterday is greater than dolarToday,then,it is printed expression that decreasing arrow.
+
 elif dolarYesterday > dolarToday:
     print("decreasing arrow")
+
+#Otherwise,it is printed expression that equal arrow.
 
 else:
     print("equal arrow")
 
-print("finished")
+print("finished") # It is printed expression that finished.
