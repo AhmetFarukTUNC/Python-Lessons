5ba97a34a8ec5de75a92ad6dd8f10bf1fb7d4ccf
YAZILIMCIABII <atunc8402@gmail.com>
2024-04-01 00:05:19 +0300

    We updated PythonLessons Repo.

--- mostnumber/mn.py
+++ mostnumber/mn.py
@@ -1,16 +1,22 @@
-number1 = int(input("Number 1 : "))
+number1 = int(input("Number 1 : ")) # We take a number from user assign to number1 variable.
 
-number2 = int(input("Number 2 : "))
+number2 = int(input("Number 2 : ")) # We take a number from user assign to number2 variable.
 
-number3 = int(input("Number 3 : "))
+number3 = int(input("Number 3 : ")) # We take a number from user assign to number3 variable.
+
+# If number1 greater than number2 and number3,then,we assigned number1 to biggestNumber.
 
 if (number1 >= number2) and number1 >= number3:
    biggestNumber = number1
 
+# If number2 greater than number1 and number3,then,we assigned number2 to biggestNumber.
+
 if (number2 >= number1) and number2 >= number3:
    biggestNumber = number2
+
+# If number3 greater than number1 and number2,then,we assigned number3 to biggestNumber.
 
 if (number3 >= number1) and number3 >= number2:
    biggestNumber = number3
 
-print("En Büyük :",biggestNumber)
+print("Biggest Number :",biggestNumber) # We printed biggestNumber variable.
---
+++ mostnumber/readMe.md
@@ -0,0 +1,25 @@
+# Finding the Biggest Number among Three Numbers
+
+## This Python script takes three numbers as input from the user and determines the biggest number among them.
+
+## How to Use:
+
+### 1. Run the script.
+### 2. Enter three numbers when prompted.
+### 3. The script will output the biggest number among the three.
+
+## Example:
+
+### Number 1: 5
+### Number 2: 9
+### Number 3: 3
+### Biggest Number: 9
+
+
+## Requirements:
+
+### - Python 3.x
+
+## Author:
+
+### AhmetFarukTUNC
